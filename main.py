1aca9c55f987cf5e6a69413541b693a4b4397efe
cayis101295 <cayis.cata@gmail.com>
2023-04-11 20:59:03 -0500

    Ajuste metodos carga

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def run():
     data_amazon_reviews = read_data.parse_csv_data_frame("amazon_reviews.csv", ['reviewerName', 'overall', 'reviewText', 'reviewTime'])
     conn = conn_data_base.connect_data_base()
     conn_data_base.load_data_amazon(data_amazon_reviews)
-    data_amazon_ufo= read_data.parse_csv_data_frame("nuforc_reports.csv", ['level_0', 'index', 'stats'])
-    conn_data_base.load_data_ufo(data_amazon_reviews)
+    data_amazon_ufo = read_data.parse_csv_data_frame("nuforc_reports.csv", ['level_0', 'index', 'stats'])
+    conn_data_base.load_data_ufo(data_amazon_ufo)
 
 if __name__ == "__main__":
     run()
